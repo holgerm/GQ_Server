d6e5aadb2200f7e2b5fb18cb05942afa76efcddb
questmill <git@quest-mill.com>
2019-04-17 23:22:35 +0200

    formatted

--- app/models/GameParts/AttributeType.java
+++ app/models/GameParts/AttributeType.java
@@ -10,189 +10,213 @@ import javax.persistence.OneToOne;
 
 import play.db.ebean.Model;
 
-
-
 @Entity
 public class AttributeType extends Model {
 
-    @Id
-    private Long id;
-    
-    private String name;
-    private String xmltype;
-    private boolean editable;
-    private boolean show;
-    private boolean canHaveAbstractValue;
-    private String defaultvalue;
-    
-    
-    @OneToOne
-    private ObjectReference linkto;
-    
+	@Id
+	private Long id;
 
-    private String premiumcodes;
+	private String name;
+	private String xmltype;
+	private boolean editable;
+	private boolean show;
+	private boolean canHaveAbstractValue;
+	private String defaultvalue;
 
-    
-    private boolean optional;
-    private String filetype;
-    private String editortype;
-    private String mimetype;
-    
-    private Long sort;
-   
-    private boolean showinparent;
-    
-    @ManyToMany
-    private List<SortedString> possibleValues;
+	@OneToOne
+	private ObjectReference linkto;
 
-    public AttributeType(String n, String xml, String ft){
-     
-    	name = n;
-    	xmltype = xml;
-    	
-    	
-    	/* CHECK IF FILETYPE IS DEFINED?
-    	 * 
-    	 */
-    	
-    	filetype = ft;
-    	
-    	editortype = ft;
+	private String premiumcodes;
 
-    	editable = true;
-    	show = true;
-    	optional = false;
-    	showinparent = false;
-    	canHaveAbstractValue = true;
-    		
-    	defaultvalue = "";
-    	mimetype = "none";
-    	
+	private boolean optional;
+	private String filetype;
+	private String editortype;
+	private String mimetype;
 
-    }
+	private Long sort;
 
+	private boolean showinparent;
 
-    
-    /* SETTER
-     * 
-     */
+	@ManyToMany
+	private List<SortedString> possibleValues;
 
-    public void setShowInParent(boolean x){ showinparent = x; }
-    public void setMimeType(String x){ mimetype = x; }
-    public void setEditable(boolean x){ editable = x; }
-    public void setVisibility(boolean x){ show = x; }
-    public void setDefaultValue(String x){ defaultvalue = x; }
-    public void addPossibleValue(String x){ SortedString nss = new SortedString(x); nss.save(); possibleValues.add(nss); }
-    
-    
-    		/* DELETE POSSIBLE VALUE
-    		 * 
-    		 */
-    
-    
-    
-    
-    
-    
-    /* GETTER
-     * 
-     */
-    
+	public AttributeType(String n, String xml, String ft) {
 
-    public Long getId(){
-        return id;
-    }
+		name = n;
+		xmltype = xml;
 
-    public boolean isVisibleInParent(){ return showinparent; }
-public String getMimeType(){ if(mimetype != null){ return mimetype; } else { return "null"; } }
-    public String getXMLType(){ return xmltype; }
-    public boolean getVisibility(){ return show; }
-    public boolean isEditable(){ return editable; }
-    public String getDefaultValue(){ return defaultvalue; }
-    
-    
-    public List<String> getPossibleValues(){ 
-    	List<String> possibleValuesAsString = new ArrayList<String>();
-    	for(SortedString ss: possibleValues){ possibleValuesAsString.add(ss.getString()); }
-    	
-    	return possibleValuesAsString; }
-    public boolean hasPossibleValues(){ if(possibleValues.isEmpty()){ return false; } else { return true; }  }
-   
+		/*
+		 * CHECK IF FILETYPE IS DEFINED?
+		 * 
+		 */
 
-    
-    
-    public String getFileType(){ if(filetype.equals("seconds")){ return "int"; } else if(filetype.equals("stringseconds")){ return "String"; } {return filetype;} }
-    
-    public String getRealFileType(){ return filetype; }
-    
-    
-    public Attribute createMe(){
-    	
-    	
-    	Attribute a = new Attribute(this);
-    	a.save();
-    	
-    	a.setValue(defaultvalue);
-    	
-    	
+		filetype = ft;
 
-    	
-    	
-    	return a;
-    	
-    }
-    
-    
-    
-    
+		editortype = ft;
 
+		editable = true;
+		show = true;
+		optional = false;
+		showinparent = false;
+		canHaveAbstractValue = true;
 
-    public static final Finder<Long, AttributeType> find = new Finder<Long, AttributeType>(
-            Long.class, AttributeType.class);
+		defaultvalue = "";
+		mimetype = "none";
+
+	}
+
+	/*
+	 * SETTER
+	 * 
+	 */
+
+	public void setShowInParent(boolean x) {
+		showinparent = x;
+	}
+
+	public void setMimeType(String x) {
+		mimetype = x;
+	}
+
+	public void setEditable(boolean x) {
+		editable = x;
+	}
+
+	public void setVisibility(boolean x) {
+		show = x;
+	}
+
+	public void setDefaultValue(String x) {
+		defaultvalue = x;
+	}
+
+	public void addPossibleValue(String x) {
+		SortedString nss = new SortedString(x);
+		nss.save();
+		possibleValues.add(nss);
+	}
+
+	/*
+	 * DELETE POSSIBLE VALUE
+	 * 
+	 */
+
+	/*
+	 * GETTER
+	 * 
+	 */
+
+	public Long getId() {
+		return id;
+	}
+
+	public boolean isVisibleInParent() {
+		return showinparent;
+	}
+
+	public String getMimeType() {
+		if (mimetype != null) {
+			return mimetype;
+		} else {
+			return "null";
+		}
+	}
+
+	public String getXMLType() {
+		return xmltype;
+	}
+
+	public boolean getVisibility() {
+		return show;
+	}
+
+	public boolean isEditable() {
+		return editable;
+	}
+
+	public String getDefaultValue() {
+		return defaultvalue;
+	}
+
+	public List<String> getPossibleValues() {
+		List<String> possibleValuesAsString = new ArrayList<String>();
+		for (SortedString ss : possibleValues) {
+			possibleValuesAsString.add(ss.getString());
+		}
+
+		return possibleValuesAsString;
+	}
+
+	public boolean hasPossibleValues() {
+		if (possibleValues.isEmpty()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public String getFileType() {
+		if (filetype.equals("seconds")) {
+			return "int";
+		}
+
+		if (filetype.equals("stringseconds")) {
+			return "String";
+		}
+
+		return filetype;
+	}
+
+	public String getRealFileType() {
+		return filetype;
+	}
+
+	public Attribute createMe() {
+
+		Attribute a = new Attribute(this);
+		a.save();
+
+		a.setValue(defaultvalue);
+
+		return a;
+
+	}
+
+	public static final Finder<Long, AttributeType> find = new Finder<Long, AttributeType>(Long.class,
+			AttributeType.class);
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setOptional(boolean b) {
 		optional = b;
-		
-	}
-	
-	
-	public void setLink(ObjectReference o){
-		
-		linkto = o;
-		
+
 	}
 
-	
-	
-	
-	public ObjectReference getLink(){
-		
-		
-		return linkto;
-		
+	public void setLink(ObjectReference o) {
+
+		linkto = o;
+
 	}
-	
-	
-	public boolean hasLink(){
-		
-		
-		if(linkto != null){
-			
+
+	public ObjectReference getLink() {
+
+		return linkto;
+
+	}
+
+	public boolean hasLink() {
+
+		if (linkto != null) {
+
 			return true;
-			
+
 		} else {
-			
+
 			return false;
 		}
-		
+
 	}
-
-
 
 }
